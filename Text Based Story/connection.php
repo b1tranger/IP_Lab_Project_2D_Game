89d4c63b5e0baa4d0438cfe2f3953cebdf505635
BitRanger <63799660+b1tranger@users.noreply.github.com>
2025-11-13 19:58:03 +0600

    some edits

--- Text Based Story/connection.php
+++ Text Based Story/connection.php
@@ -8,3 +8,15 @@ $conn = new mysqli($name,$username,$password,$dbname);
 
 
 ?>
+
+
+<?php
+// $name="sql212.infinityfree.com";
+// $username="if0_40351225";
+// $password="grp7sesaproject";
+// $dbname="if0_40351225_game_db";
+
+// $conn = new mysqli($name,$username,$password,$dbname);
+
+
+?>
